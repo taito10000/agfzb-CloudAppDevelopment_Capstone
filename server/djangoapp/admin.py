01cadbb9de716c071c132ef35f3e1c08e2c32aea
Taito Kantomaa <taitosamuli@gmail.com>
2022-05-31 07:02:17 +0000

    djangoapp - models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,24 @@
 from django.contrib import admin
-# from .models import related models
-
+from .models import CarMake, CarModel
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+
 
 # CarModelAdmin class
 
+class CarModelAdmin(admin.ModelAdmin):
+    list_display =['name']
+
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    inlines = [CarModelInline]
+    
 
 # Register models here
+admin.site.register(CarModel, CarModelAdmin)
+admin.site.register(CarMake)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,4 +1,12 @@
-from django.db import models
+try:
+    from django.db import models
+except Exception:
+    print("There was an error loading django modules. Do you have django installed?")
+    sys.exit()
+
+from django.conf import settings
+import uuid
+
 from django.utils.timezone import now
 
 
@@ -10,6 +18,13 @@ from django.utils.timezone import now
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
 
+class CarMake(models.Model):
+    name = models.CharField(max_length=60)
+    desc = models.CharField(max_length=300)
+
+    def __str__(self):
+        txt = f"CarMake Class: {self.name}, {self.desc}"
+        return txt
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -19,6 +34,22 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+
+class CarModel(models.Model):
+
+    carMake = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=60)
+    dealerId = models.IntegerField()
+    carType = models.CharField(max_length=60)
+    year = models.DateField()
+
+    def __str__(self):
+        txt = f"{self.carMake}, {self.name}, {self.carType}, {self.year}"
+        return txt
+
+class myOwnModel(models.Model):
+    name = models.CharField(max_length=60)
+    age = models.IntegerField()
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
