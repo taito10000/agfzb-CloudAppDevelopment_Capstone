56a73bc30b1bba0a904eb934f3bf5e3ba26ffee7
Taito Kantomaa <taitosamuli@gmail.com>
2022-05-31 10:19:42 +0000

    edit eteenpain

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -3,13 +3,15 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 # from .models import related models
-# from .restapis import related methods
+from .restapis import get_dealers_from_cf, get_request, get_dealer_by_id_from_cf
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
 import logging
 import json
-
+import requests
+from .models import CarDealer
+from requests.auth import HTTPBasicAuth
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
@@ -82,14 +84,26 @@ def registration_request(request):
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
-    context = {}
     if request.method == "GET":
-        return render(request, 'djangoapp/index.html', context)
+        url = "https://608f2dc9.us-south.apigw.appdomain.cloud/ibmcapstone/dealership"
+        # Get dealers from the URL
+        dealerships = get_dealers_from_cf(url)
+        # Concat all dealer's short name
+        dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
+        # Return a list of dealer short name
+        return HttpResponse(dealer_names)
+
+
+
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
-# def get_dealer_details(request, dealer_id):
-# ...
+def get_dealer_details(request, dealer_id):
+    url = "https://us-south.functions.appdomain.cloud/api/v1/web/taito.kantomaa%40coreseer.com_dev2/ibm10final/getReviewsForDealer"
+    
+    dealers = get_dealer_by_id_from_cf(url, dealer_id)
+
+
 
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
