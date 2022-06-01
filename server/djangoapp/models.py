20c3e595b93fa39bfb9d17c2fafd10a82d139ff0
Taito Kantomaa <taitosamuli@gmail.com>
2022-06-01 12:48:05 +0000

    defaults to do

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -88,10 +88,10 @@ class DealerReview:
         self.purchase = purchase
         self.review = review
         self.purchase_date = purchase_date
-        self.car_make = car_make
+        self.car_make = car_make 
         self.car_model = car_model
         self.car_year = car_year
-        self.sentiment = sentiment
+        self.sentiment = sentiment 
         self.id = id
 
     def __str__(self):
